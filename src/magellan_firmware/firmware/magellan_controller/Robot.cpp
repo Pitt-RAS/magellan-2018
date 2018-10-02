5db2e9f147efb45cb71be138604d435188f4f90e
Quinnan-Gill <quinnan.gill@gmail.com>
2018-10-02 18:20:48 -0400

    Fixed the subscriber heap/stack error

--- src/magellan_firmware/firmware/magellan_controller/Robot.cpp
+++ src/magellan_firmware/firmware/magellan_controller/Robot.cpp
@@ -7,10 +7,10 @@ Robot::Robot(ros::NodeHandle& nh) :
     heartbeat_(),
     throttle_pwm_(ESC_PWM),
     steering_pwm_(SERVO_PWM),
-    throttle_subscriber_(new <std_msgs::Float64, Robot("/platform/throttle", &Robot::UpdateThrottle, this)),
-    throttle_percent_(0),
-    steering_subscriber_(new <std_msgs::Float64, Robot("/platform/steering", &Robot::UpdateSteering, this)),
-    steering_angle_(0) {
+    throttle_subscriber_("/platform/throttle", &Robot::UpdateThrottle, this),
+    throttle_percent_(0.0),
+    steering_subscriber_("/platform/steering", &Robot::UpdateSteering, this),
+    steering_angle_(0.0) {
         steering_pwm_.ConfigOffset(STEERING_OFFSET);
         throttle_pwm_.ConfigLowLimit(THROTTLE_MIN);
         steering_pwm_.ConfigLowLimit(STEERING_MIN);
@@ -21,32 +21,25 @@ Robot::Robot(ros::NodeHandle& nh) :
 void Robot::TeleopInit() {
 }
 
-void Robot::UpdateThrottle(const std_msgs::Float64& cmd_throttle_percent_) {
-    throttle_percent_ = cmd_throttle_percent_.data;
-}
-
-void Robot::UpdateSteering(const std_msgs::Float64& cmd_steering_angle_) {
-    steering_angle_ = cmd_steering_anlge_.data; 
-}
-
 void Robot::TeleopPeriodic() {
-    // Get the throttle percent from the Transmitter Interface
-    double throttle_percent_ = transmitter_.throttle_percent();
+    // Get the throttle percent from the Transmitter Interface and pass it to PWM
+    throttle_pwm_.Set(transmitter_.throttle_percent());
 
-    //Pass it to PWM
-    throttle_pwm_.Set(throttle_percent_);
-
-    // Get the steering angle from the Transmitter Interface
-    double steering_angle_ = transmitter_.steering_angle();
-
-    // Pass it to PWM
-    steering_pwm_.Set(steering_angle_ / 90.0);
+    // Get the steering angle from the Transmitter Interface and pass it to PWM
+    steering_pwm_.Set(transmitter_.steering_angle());
 }
 
 void Robot::AutonomousInit() {
+    steering_pwm_.Set(0.0);
+    throttle_pwm_.Set(0.0);
+
+    throttle_percent_ = 0.0;
+    steering_angle_ = 0.0;
 }
 
 void Robot::AutonomousPeriodic() {
+    throttle_pwm_.Set(throttle_percent_);
+    steering_pwm_.Set(steering_angle_);
 }
 
 void Robot::DisabledInit() {
@@ -57,6 +50,14 @@ void Robot::DisabledInit() {
 void Robot::DisabledPeriodic() {
     throttle_percent_ = 0;
     steering_angle_ = 0;
+}
+
+void Robot::UpdateThrottle(const std_msgs::Float64& cmd_throttle_percent_) {
+    throttle_percent_ = cmd_throttle_percent_.data;
+}
+
+void Robot::UpdateSteering(const std_msgs::Float64& cmd_steering_angle_) {
+    steering_angle_ = cmd_steering_angle_.data; 
 }
 
 void Robot::Update() {
@@ -87,3 +88,4 @@ void Robot::Update() {
     else if ( current_state_ == MODE_TELEOP )
         TeleopPeriodic();
 }
+
--- src/magellan_firmware/firmware/magellan_controller/Robot.h
+++ src/magellan_firmware/firmware/magellan_controller/Robot.h
@@ -8,6 +8,7 @@
 #include <ros.h>
 #include "HeartbeatLED.h"
 #include "PWM.h"
+#include <std_msgs/Float64.h>
 
 class Robot {
 public:
@@ -20,6 +21,8 @@ public:
     void DisabledInit();
     void DisabledPeriodic();
     void AlwaysPeriodic();
+    void UpdateThrottle(const std_msgs::Float64& cmd_throttle_percent_);
+    void UpdateSteering(const std_msgs::Float64& cmd_steering_angle_);
 
     void Update();
 private:
