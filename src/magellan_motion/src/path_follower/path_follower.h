b18f730b615d98f87bca5e0e4cc79f5bbfc5b0c8
Andrew Lobos <andrew@lobos.me>
2018-10-30 16:35:42 -0400

    WIP

--- src/magellan_motion/src/path_follower/path_follower.h
+++ src/magellan_motion/src/path_follower/path_follower.h
@@ -3,14 +3,22 @@
 
 #include <ros/ros.h>
 #include <nav_msgs/Path.h>
+#include <nav_msgs/Odometry.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/utils.h>
 
 class PathFollower {
     public:
         PathFollower(ros::NodeHandle& nh);
     private:
         ros::NodeHandle& nh_;
+        tf2_ros::Buffer tf_buffer_;
+        tf2_ros::TransformListener tf_listener_;
+
         ros::Subscriber path_subscriber_;
+
         nav_msgs::Path::ConstPtr current_path_;
+        tf2::Vector3 current_position_;
         int path_start_index_;
 
         void UpdatePath(nav_msgs::Path::ConstPtr path);
