3a696b82196e4e9479ff32c70f573932ffc23d00
Andrew Lobos <andrew@lobos.me>
2018-11-01 19:31:24 -0400

    WIP

--- src/magellan_motion/src/path_follower/path_follower_node.cpp
+++ src/magellan_motion/src/path_follower/path_follower_node.cpp
@@ -5,7 +5,30 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "path_follower");
 
     ros::NodeHandle nh;
+    ros::NodeHandle private_nh("~");
+
+    double rate_hz = 100;
+    private_nh.getParam("rate", rate_hz);
+    ros::Rate rate(rate_hz);
+
+    double lookahead_distance;
+    if ( !private_nh.getParam("lookahead_distance", lookahead_distance) ) {
+        ROS_ERROR("lookahead_distance param unset");
+        ros::shutdown();
+        return 0;
+    }
+
+    double discretization;
+    if ( !private_nh.getParam("discretization", lookahead_distance) ) {
+        ROS_ERROR("discretization param unset");
+        ros::shutdown();
+        return 0;
+    }
+
+    PathFollower path_follower(nh, discretization, lookahead_distance);
 
     while (ros::ok()) {
+        path_follower.Update();
+        rate.sleep();
     }
 }
