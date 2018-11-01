fc7829ed158720d2b071226572a093eee3a13b31
Andrew Lobos <andrew@lobos.me>
2018-11-01 19:31:24 -0400

    WIP

--- src/magellan_motion/src/path_follower/path_follower.cpp
+++ src/magellan_motion/src/path_follower/path_follower.cpp
@@ -1,50 +1,62 @@
 #include "path_follower.h"
+#include <std_msgs/Float64.h>
+//#include <vizualization_msgs/Marker.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <optional>
 
 static inline double L1Norm(const geometry_msgs::PoseStamped& pose) {
     return std::abs(pose.pose.position.x) + std::abs(pose.pose.position.y);
 }
 
 
-PathFollower::PathFollower(ros::NodeHandle& nh) :
+PathFollower::PathFollower(ros::NodeHandle& nh, double discretization, double lookahead_distance) :
     nh_(nh),
     tf_buffer_(),
     tf_listener_(tf_buffer_),
     path_subscriber_(nh.subscribe("/path", 10, &PathFollower::UpdatePath, this)),
-    path_start_index_(0) {
+    velocity_publisher_(nh.advertise<std_msgs::Float64>("/cmd_velocity", 10)),
+    turning_radius_publisher_(nh.advertise<std_msgs::Float64>("/cmd_turning_radius", 10)),
+    path_start_index_(0),
+    discretization_(discretization),
+    lookahead_distance_(lookahead_distance) {
 }
 
 void PathFollower::UpdatePath(nav_msgs::Path::ConstPtr path) {
-    auto transform = tf_buffer_.lookupTransform((*path).header.frame_id, "base_link", ros::Time::now(), ros::Duration(1.0));
+    current_path_ = path;
+    path_start_index_ = 0;
+}
 
-    geometry_msgs::PoseSamped closest_pose;
-    double closest_pose_distance;
-    auto it = path.poses.begin();
-    for ( it += path_start_index_; it != path.poses.end(); ++it) {
+void PathFollower::Update() {
+    auto transform = tf_buffer_.lookupTransform((*current_path_).header.frame_id, "base_link", ros::Time::now(), ros::Duration(1.0));
+
+    int lookahead_points = lookahead_distance_ / discretization_;
+
+    geometry_msgs::PoseStamped closest_pose;
+    auto it = (*current_path_).poses.begin();
+    std::optional<geometry_msgs::PoseStamped> closest_point;
+    for ( it += path_start_index_; it != (*current_path_).poses.end(); ++it) {
         geometry_msgs::PoseStamped temp;
         tf2::doTransform(*it, temp, transform);
-        double pose_distance = L1Norm(pose);
-        if ( pose_distance < closest_pose_distance ) {
-            closest_pose_distance = pose_distance;
-            path_start_index_ = it - path.poses.begin();
-        }
+        if ( closest_point.has_value() && L1Norm(temp) > L1Norm(*closest_point) )
+            break;
+        closest_point = temp;
     }
-    current_path_ = path;
+    path_start_index_ = std::distance((*current_path_).poses.begin(), it);
+
+    it += std::min(static_cast<int>(std::distance(it, (*current_path_).poses.end())), lookahead_points);
+
+    geometry_msgs::PoseStamped lookahead_pose;
+    tf2::doTransform(*it, lookahead_pose, transform);
+    double turning_radius = (lookahead_distance_*lookahead_distance_) / (2.0 * lookahead_pose.pose.position.x);
+
+    static std_msgs::Float64 turning_command;
+    turning_command.data = turning_radius;
+
+    turning_radius_publisher_.publish(turning_command);
 }
 
 
-void PathFollower::Update() {
-    auto transform = tf_buffer_.lookupTransform((*path).header.frame_id, "base_link", ros::Time::now(), ros::Duration(1.0));
-
-    geometry_msgs::PoseSamped closest_pose;
-    double last_pose_distance = 999;
-    auto it = path.poses.begin();
-    for ( it += path_start_index_; it != path.poses.end(); ++it) {
-        geometry_msgs::PoseStamped temp;
-        tf2::doTransform(*it, temp, transform);
-        double pose_distance = L1Norm(*it);
-        if ( pose_distance > last_pose_distance ) {
-            break;
-        }
-    }
+void PathFollower::UpdateMarkers() {
+//    static vizualization_msgs::Marker closest_point_marker;
+//    static vizualization_msgs::Marker lookahead_marker;
 }
--- src/magellan_motion/src/path_follower/path_follower.h
+++ src/magellan_motion/src/path_follower/path_follower.h
@@ -9,19 +9,27 @@
 
 class PathFollower {
     public:
-        PathFollower(ros::NodeHandle& nh);
+        PathFollower(ros::NodeHandle& nh, double discretization, double lookahead_distance);
+        void Update();
     private:
         ros::NodeHandle& nh_;
         tf2_ros::Buffer tf_buffer_;
         tf2_ros::TransformListener tf_listener_;
+        ros::Rate debug_marker_rate_;
 
         ros::Subscriber path_subscriber_;
+        ros::Publisher velocity_publisher_;
+        ros::Publisher turning_radius_publisher_;
 
         nav_msgs::Path::ConstPtr current_path_;
         tf2::Vector3 current_position_;
         int path_start_index_;
 
+        double discretization_;
+        double lookahead_distance_;
+
         void UpdatePath(nav_msgs::Path::ConstPtr path);
+        void UpdateMarkers();
 };
 
 #endif
