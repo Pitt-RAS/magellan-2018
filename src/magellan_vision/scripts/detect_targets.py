29921b5354c9a35c062aa9067f3fcc02864dea64
Kaylene Stocking <viola.sox@gmail.com>
2019-03-03 18:16:37 +0000

    all 3 targets detected

--- src/magellan_vision/scripts/detect_targets.py
+++ src/magellan_vision/scripts/detect_targets.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 from scipy import ndimage
+import math
 
 # Temporary for testing - hues from the images in rules document
 hueArch = 39
@@ -18,10 +19,10 @@ cannyThresh = 100
 houghArchThresh = 70
 verticalArchThresh = 10
 houghPoleThresh = 40
-verticalPoleThresh = 10
+verticalPoleThresh = 5
 # I think these should be the same for all targets
 # Sat should be wide for various outdoors conditions
-satMin = 100
+satMin = 50
 satMax = 255
 # Low val = less color - raise min to reduce risk of picking up non-target objects
 valMin = 0
@@ -30,6 +31,7 @@ valMax = 255
 detectArch = True
 detectRamp = True
 detectPole = True
+debugOutput = False
 
 # Get the current frame from ROS
 if detectArch:
@@ -53,14 +55,16 @@ if detectArch:
                     sameLine = True
             if not sameLine:
                 poles.append(x)
-                cv.line(frame, (line[0], line[1]), (line[2], line[3]), (0,0,255), 3)
-                cv.imshow("lines", frame)
-                cv.waitKey(0)    
+                if debugOutput:
+                    cv.line(frame, (line[0], line[1]), (line[2], line[3]), (0,0,255), 3)
+                    cv.imshow("lines", frame)
+                    cv.waitKey(0)    
 
     if len(poles) == 2:
         archMidPt = (poles[0] + poles[1]) / 2
         archWidth = abs(poles[0] - poles[1])
-        print('Center of arch is {}, arch width is {}'.format(archMidPt, archWidth))
+        if debugOutput:
+            print('Center of arch is at x={}, arch width is {}'.format(archMidPt, archWidth))
 
 if detectRamp:
     frame = cv.imread('Ramp.png', cv.IMREAD_COLOR)
@@ -69,55 +73,44 @@ if detectRamp:
     rampThreshold = cv.inRange(frameHSV, (hueMinRamp, satMin, valMin), (hueMaxRamp, satMax, valMax))
     center = ndimage.measurements.center_of_mass(rampThreshold)
     centerImage = cv.circle(frame, (int(center[1]), int(center[0])), 10, (0,0,255), 3)
-    cv.imshow("threshold", centerImage)
-    cv.waitKey(0)
+    if debugOutput:
+        print('There is a ramp centered at x={}, y={}'.format(center[1], center[0]))
+        cv.imshow("threshold", centerImage)
+        cv.waitKey(0)
 
 if detectPole:
     frame = cv.imread('Stanchion2.png', cv.IMREAD_COLOR)
     frameHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     poleThreshold = cv.inRange(frameHSV, (hueMinPole, satMin, valMin), (hueMaxPole, satMax, valMax))
-    cv.imshow("threshold", poleThreshold)
     lines = cv.HoughLinesP(poleThreshold, 1, np.pi/180, houghPoleThresh)
-    for line in lines:
-        line = line[0]
-        cv.line(frame, (line[0], line[1]), (line[2], line[3]), (0,0,255), 3)
-    cv.imshow("lines", frame)
-    cv.waitKey(0)
 
     poles = [] 
     poleLengths = []
+    finalLines = []
     for line in lines:
         line = line[0]
         if abs(line[0] - line[2]) < verticalPoleThresh:
             x = (line[0] + line[2]) / 2
             sameLine = False
-            myLength = sqrt((line[0] - line[2])**2 + (line[1] - line[3])**2)
-            for i in range(len(poles))
-                if abs(x - poles[i]) < 10:
+            myLength = math.sqrt((line[0] - line[2])**2 + (line[1] - line[3])**2)
+            for i in range(len(poles)):
+                if abs(x - poles[i]) < 20:
                     sameLine = True
                     if myLength > poleLengths[i]:
                         poles[i] = x
                         poleLengths[i] = myLength
+                        finalLines[i] = line
 
             if not sameLine:
                 poles.append(x)
-                cv.line(frame, (line[0], line[1]), (line[2], line[3]), (0,0,255), 3)
-                cv.imshow("lines", frame)
-                cv.waitKey(0)
+                poleLengths.append(myLength)
+                finalLines.append(line)
 
-    for pole in poles:
-        print('There is a pole at x={}'.format(pole))
-    
-    #detector = cv.SimpleBlobDetector_create()
-    #keypoints = detector.detect(rampThreshold)
-    #for kp in keypoints:
-    #    x, y = kp.pt
-    #    imKeypoints = cv.circle(frame, (int(x), int(y)), 10, (0,0,255), 3)
-    #cv.imshow("blobs", imKeypoints)
-    #cv.waitKey(0)
+    if debugOutput:
+        for line in finalLines:
+            cv.line(frame, (line[0], line[1]), (line[2], line[3]), (0,0,255), 3)
+            cv.imshow("lines", frame)
+            cv.waitKey(0)
 
-# # Ramp detection should find the center of the blob
-# 
-# poleThreshold = cv.inRange(frameHSV, (hueMinPole, satMin, valMin), (hueMaxPole, satMax, valMax))
-
-# Pole detection should find the center of the blob
+        for pole in poles:
+            print('There is a pole at x={}'.format(pole))
